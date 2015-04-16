377570e2b665115422f2d263b5b80760eb196a5a
hollyhudson <hollyf@swcp.com>
2015-04-16 17:36:11 -0400

    Added rainbowCycle to first_program

--- first_program/first_program.ino
+++ first_program/first_program.ino
@@ -20,6 +20,34 @@ Adafruit_NeoPixel pixels = Adafruit_NeoPixel(
 	NEO_GRB + NEO_KHZ800		// type of pixels?
 );
 
+// This is code was adapted from code from Adafruit
+// Makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait) {
+	 uint16_t i, j;
+
+	for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+		for(i=0; i< pixels.numPixels(); i++) {
+			pixels.setPixelColor(i, Wheel(((i * 256 / pixels.numPixels()) + j) & 255));
+    		}
+		pixels.show();
+		delay(wait);
+	}
+}
+
+// This is code from Adafruit
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+	if(WheelPos < 85) {
+		return pixels.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+	} else if(WheelPos < 170) {
+		WheelPos -= 85;
+		return pixels.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+	} else {
+		WheelPos -= 170;
+		return pixels.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+	}
+}
 
 /*
  * This function is called when the FLORA "boots up" and is
